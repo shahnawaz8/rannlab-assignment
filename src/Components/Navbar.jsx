37b4153781f254676cffe87262cd0b3a06ab2ac0
shahnawaz8 <32464723+shahnawaz8@users.noreply.github.com>
2022-06-25 22:34:53 +0530

    add navbar and home page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,33 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { getUser } from '../Redux/GetUser/action';
 import { logout } from '../Redux/Login/action';
 import './home.css';
 
 export const Navbar = ()=>{
     const {token,isAuthenticate} = useSelector((store)=>store.login);
+    const {username,email} = useSelector((store)=>store.user);
+    
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    console.log("token",token)
     const handlelogout = ()=>{
-            dispatch(logout())
+        dispatch(logout())
     }
+    useEffect(()=>{
+        if(isAuthenticate){
+            getUser(token,dispatch);
+        }
+    },[])
+    
+    console.log({token,isAuthenticate,username,email});
     return (
         <>
         <div className='main'>
             <div onClick={()=>navigate('/')}>Home</div>
             <div onClick={()=>navigate('/login')}>Login</div>
             <div onClick={()=>navigate('/register')}>Signup</div>
-            <div onClick={handlelogout}>Logout</div>
+            <div onClick={handlelogout}>{isAuthenticate?username:"logout"}</div>
         </div>
 
         </>
